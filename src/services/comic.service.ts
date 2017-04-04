321935366bc3dd2f61f1698ddfdd04d46d138d4d
Yordan Vera <yordanveracastillo@gmail.com>
2017-04-04 15:51:30 -0300

    servicio comics
    se agrega el servicio encargado de obtener los cómics desde la API de
    marvel.

---
+++ src/services/comic.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Http, RequestOptions, URLSearchParams } from '@angular/http';
+
+@Injectable()
+export class ComicService {
+    static URL: string = 'http://gateway.marvel.com/v1/public/comics';
+
+    constructor (private http: Http) {
+        console.log('constructing ComicService');
+    }
+    getComics(){
+
+        const search: URLSearchParams = new URLSearchParams();
+        search.set('ts', '1');
+        search.set('apikey', 'ca9bc801b4851bb9a88bd913281bde1f');
+        search.set('hash', '62c6854ae7b03783c07658effef3d43b');
+        return this.http
+        .get(ComicService.URL, new RequestOptions({search}));
+    }
+}
