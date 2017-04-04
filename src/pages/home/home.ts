eb4d3777051dbc663cdcbabcaf0a9feebd8d75c8
Yordan Vera <yordanveracastillo@gmail.com>
2017-04-04 15:53:03 -0300

    se agrega el servicio de comic a la página principal
    el controlador de la página principal obtiene la lista de comics a
    través del servicio comicService.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { NavController } from 'ionic-angular';
+import { ComicService } from '../../services/comic.service';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html',
+  providers:[ComicService]
 })
-export class HomePage {
+export class HomePage implements OnInit{
+  public listComics;
 
-  constructor(public navCtrl: NavController) {
-    
+  constructor(public navCtrl: NavController,
+              private comicService: ComicService) {
+                    var info = this.comicService.getComics();
+                    info.subscribe(
+                      data => {
+                        this.listComics = data.json().data.results;
+                      }
+                    );
   }
+  ngOnInit(){
 
+  }
 }
